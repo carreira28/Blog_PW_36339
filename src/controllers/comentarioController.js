af85ebd4b2563644c5731591db4af9acccc0613c
carreira28 <carreira907@gmail.com>
2026-04-18 16:00:16 +0100

    .

--- src/controllers/comentarioController.js
+++ src/controllers/comentarioController.js
@@ -38,16 +38,3 @@ exports.apagar = async (req, res, next) => {
  res.status(204).send();
  } catch (err) { next(err); }
 };
-const express = require('express');
-const { body } = require('express-validator');
-const ctrl = require('../controllers/comentarioController');
-const { proteger } = require('../middleware/auth');
-const validar = require('../middleware/validar');
-const router = express.Router({ mergeParams: true });
-router.get('/', ctrl.listar);
-router.post('/', proteger,
- [body('conteudo').notEmpty().withMessage('Conteúdo obrigatório')],
- validar, ctrl.criar
-);
-router.delete('/:id', proteger, ctrl.apagar);
-module.exports = router;
